15dd2daa959f3255bb28634013b9139e94779306
Brayden Curtis <braydencurtis@gmail.com>
2020-04-05 15:01:43 -0700

    Added toggle func to nav closed class

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
 var hamburger = document.getElementById("hamburger");
 var nav = document.getElementById("nav");
-nav.style.right = "100%";
 var navPosition = nav.style.right;
-
-
-console.log(navPosition);
-nav.style.right = navPosition;
+console.log(nav.style.right);
 
 hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("is-active");
+    nav.classList.toggle("closed-nav");
     
-    if (navPosition == "100%") {
-        navPosition = "0";
-    } else {
-        navPosition = "100%";
-    };
-    nav.style.right = navPosition;
+    
 });
